0d0d4c9c00949d82b2999658ab250cda8c20e8e9
aveith <alexandre.da-silva-veith@inria.fr>
2019-06-21 10:53:21 +0200

    Talkmap corrections

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,42 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Porto Alegre, Brazil",
+    -30.0324999,
+    -51.2303767
+  ],
+  [
+    "Lyon, France",
+    45.6963425,
+    4.73594802991681
+  ],
+  [
+    "Las Vegas, US",
+    36.1662859,
+    -115.149225
+  ],
+  [
+    "Zhejiang, China",
+    29.0000001,
+    119.9999999
+  ],
+  [
+    "Antibes, France",
+    43.5836,
+    7.10905
+  ],
+  [
+    "Le Pleynet, France",
+    45.2803359,
+    6.0736956
+  ],
+  [
+    "Sao Leopoldo, Brazil",
+    -29.7544405,
+    -51.1516497
+  ],
+  [
+    "Larnaca, Cyprus",
+    34.9172012,
+    33.6378531
+  ]
+];
