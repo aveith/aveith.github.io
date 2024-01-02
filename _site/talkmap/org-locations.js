93235f8b6e8b4a1657f894e3775dbdc2888e299a
dasilvav <alexandre.da_silva_veith@nokia-bell-labs.com>
2024-01-02 13:58:47 +0100

    upd

--- _site/talkmap/org-locations.js
+++ _site/talkmap/org-locations.js
@@ -1,33 +1,23 @@
 var addressPoints = [
   [
-    "Porto Alegre, Brazil",
-    -30.0324999,
-    -51.2303767
+    "Las Vegas, US",
+    36.1672559,
+    -115.148516
+  ],
+  [
+    "Larnaca, Cyprus",
+    34.9236095,
+    33.6236184
+  ],
+  [
+    "Toronto, Canada",
+    43.6534817,
+    -79.3839347
   ],
   [
     "Lyon, France",
-    45.6963425,
-    4.73594802991681
-  ],
-  [
-    "Las Vegas, US",
-    36.1662859,
-    -115.149225
-  ],
-  [
-    "Zhejiang, China",
-    29.0000001,
-    119.9999999
-  ],
-  [
-    "Antibes, France",
-    43.5836,
-    7.10905
-  ],
-  [
-    "Le Pleynet, France",
-    45.2803359,
-    6.0736956
+    45.7578137,
+    4.8320114
   ],
   [
     "Kyoto, Japan",
@@ -35,18 +25,48 @@ var addressPoints = [
     135.7556075
   ],
   [
+    "Madrid, Spain",
+    40.4167047,
+    -3.7035825
+  ],
+  [
     "Sao Leopoldo, Brazil",
     -29.7544405,
     -51.1516497
   ],
   [
-    "Toronto, Canada",
-    43.653963,
-    -79.387207
+    "Zhejiang, China",
+    29.0000001,
+    119.9999999
   ],
   [
-    "Larnaca, Cyprus",
-    34.9236095,
-    33.6236184
+    "Le Pleynet, France",
+    45.2723252,
+    6.0555898
+  ],
+  [
+    "Louvain-la-Neuve, Belgium",
+    50.6682012,
+    4.6128839
+  ],
+  [
+    "Hainan, China",
+    19.2000001,
+    109.5999999
+  ],
+  [
+    "Montr\u00e9al, Qu\u00e9bec, Canada",
+    45.5031824,
+    -73.5698065
+  ],
+  [
+    "Antibes, France",
+    43.5812868,
+    7.1262071
+  ],
+  [
+    "Porto Alegre, Brazil",
+    -30.0324999,
+    -51.2303767
   ]
 ];
